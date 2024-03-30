3faff3cdc171d4366558cc6351e8b89aa3eb5467
alfredo desde lenovito <alfredoherrerabrice@gmail.com>
2024-03-30 17:36:32 -0500

    inicio de ejercicio dos

---
+++ ejercicio uno/ejerciciouno.js
@@ -0,0 +1,4 @@
+let mensajeDeBienvenida="¡Bienvenida y bienvenido a nuestro sitio web!";
+alert(mensajeDeBienvenida);
+/* EJERCICIO 1.	Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".*/
+
