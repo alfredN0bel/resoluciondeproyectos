961842fb0b7be0a2eb40f3e7e43ae64b34d4b752
alfredo desde lenovito <alfredoherrerabrice@gmail.com>
2024-03-31 12:14:39 -0500

    fin de ejercicio 4

---
+++ Ejercicio cuatro/ejercicioCuatro.js
@@ -0,0 +1,12 @@
+/*Establece una variable numeroDeVentas y asígnale el valor 50.
+5.	Establece una variable saldoDisponible y asígnale el valor 1000.
+6.	Muestra una alerta con el texto "¡Error! Completa todos los campos".
+7.	Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .
+*/
+let numeroDeVentas = 50;
+alert(numeroDeVentas);
+let saldoDisponible = 100;
+alert(saldoDisponible);
+alert("¡Error! Completa todos los campos");
+let mensajeDeError = "¡Error! Completa todos los campos";
+alert(mensajeDeError)
