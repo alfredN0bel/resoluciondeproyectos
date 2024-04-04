97ae5d0a6205557f55c3e857527dc3001555c64b
alfredo desde lenovito <alfredoherrerabrice@gmail.com>
2024-04-03 19:46:43 -0500

    avance en el index 6

--- ejercicio seis/ejercicioSeis.js
+++ ejercicio seis/ejercicioSeis.js
@@ -52,3 +52,89 @@ function fifthExercise(){
         alert("Intentalo nuevamente para ganar.")
     }
 }
+// SEGUNDO GRUPO DE EJERCICIOS.
+// Crea un programa que utilice console.log para mostrar un mensaje de bienvenida.
+function sixthExercise(){
+    let mensaje="bienvenida";
+    console.log(mensaje)
+}
+//Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza console.log para mostrar el mensaje "¡Hola, [tu nombre]!" en la consola del navegador.
+function seventhExercise(){
+    let nombre= "Alfredo";
+    console.log(`"¡Hola,${nombre}"`)
+}
+//Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza alert para mostrar el mensaje "¡Hola, [tu nombre]!".
+function eighthExercise(){
+    let nombre="Alfredo"
+    alert(`¡Hola,${nombre}!`);
+}
+
+//Utiliza prompt y haz la siguiente pregunta: ¿Cuál es el lenguaje de programación que más te gusta?. Luego, almacena la respuesta en una variable y muestra la respuesta en la consola del navegador.
+function ninthExercise(){
+    let pregunta=prompt("¿Cuál es el lenguaje de programación que más te gusta?.")
+    console.log(pregunta)
+}
+
+//Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. Luego, realiza la suma de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". Utiliza console.log para mostrar el mensaje "La suma de [valor1] y [valor2] es igual a [resultado]." en la consola.
+function tenthExercise(){
+    let valor1 = 99990;
+    let valor2 = 152533;
+    let resultado = valor1+valor2;
+    console.log(`La suma de ${valor1} y ${valor2} es igual a ${resultado}.`)
+    
+
+}
+
+//Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. Luego, realiza la resta de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". Utiliza console.log para mostrar el mensaje "La diferencia entre [valor1] y [valor2] es igual a [resultado]." en la consola.
+function eleventhExercise(){
+    let valor1 = 8526790;
+    let valor2 = 9152533;
+    let resultado = valor1-valor2;
+    console.log(`La diferencia de ${valor1} y ${valor2} es igual a ${resultado}.`)
+
+}
+
+//Pide al usuario que ingrese su edad con prompt. Con base en la edad ingresada, utiliza un if para verificar si la persona es mayor o menor de edad y muestra un mensaje apropiado en la consola.
+function twelfthExercise(){
+    let edadDeUsuario=prompt("Ingrese su edad")
+    if(edadDeUsuario<18){
+        console.log("Usted es menor de Edad")
+    } else if(edadDeUsuario>=18){
+        console.log("Usted puede utilizar el servicio solicitado")
+    } else{
+        console.log("el valor ingresado no coincide con lo solicitado")
+    }
+}   
+
+
+//Crea una variable "numero" y solicita un valor con prompt. Luego, verifica si es positivo, negativo o cero utilizando un if-else y muestra el mensaje correspondiente.
+function thirteenth(){
+    let numero = parseInt(prompt("ingrese un numero por favor"))
+    if (numero>0) {
+        alert("el numero ingresado es positivo")        
+    } else if (numero<0){
+        alert("el numero ingresado es negativo")
+    }else if(numero=0){
+        alert("el numero ingresado es cero")
+    }else{
+        alert("el valor ingresado no es un numero")
+    }
+}
+
+//Utiliza un bucle while para mostrar los números del 1 al 10 en la consola.
+function fourteenthExercise(){
+    let contador=1
+    while(contador<=10){
+        console.log(contador)
+        contador=contador+1
+        
+    }
+}
+
+//Crea una variable "nota" y asígnale un valor numérico. Utiliza un if-else para determinar si la nota es mayor o igual a 7 y muestra "Aprobado" o "Reprobado" en la consola.
+
+//Utiliza Math.random para generar cualquier número aleatorio y muestra ese número en la consola.
+
+//Utiliza Math.random para generar un número entero entre 1 y 10 y muestra ese número en la consola.
+
+//Utiliza Math.random para generar un número entero entre 1 y 1000 y muestra ese número en la consola.
